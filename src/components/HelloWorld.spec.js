00da42472e8e6a4d355c8118e4246e96d7158e14
gsuke <54441258+gsuke@users.noreply.github.com>
2023-02-12 01:14:13 +0900

    chore: reset

--- src/components/HelloWorld.spec.js
+++
@@ -1,9 +0,0 @@
-import { expect, it } from "vitest";
-import { render, screen } from "@testing-library/vue";
-import HelloWorld from "./HelloWorld.vue";
-
-it("sample test", () => {
-  render(HelloWorld, { props: { msg: "Hello Vue" } });
-  const header = screen.queryByRole("heading", { name: "Hello Vue" });
-  expect(header).toBeTruthy();
-});
