85b8c7a7f162b9c871643e49ceae6dd92429d1ab
gsuke <54441258+gsuke@users.noreply.github.com>
2023-02-12 21:08:10 +0900

    feat: add TodoItem

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
-<script></script>
+<script>
+import TodoItem from "./components/TodoItem.vue";
+
+const App = {
+  components: {
+    TodoItem,
+  },
+};
+
+export default App;
+</script>
 
 <template>
-  <div class="dropdown dropdown-open">
-    <label tabindex="0" class="btn m-1">Click</label>
-    <ul
-      tabindex="0"
-      class="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow"
-    >
-      <li><a>Item 1</a></li>
-      <li><a>Item 2</a></li>
-    </ul>
-  </div>
+  <TodoItem />
 </template>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <label :for="uid" class="label justify-start">
+    <input :id="uid" type="checkbox" checked="false" class="checkbox" />
+    <div class="label-text ml-2">Sample Task</div>
+  </label>
+</template>
+
+<script>
+const TodoItem = {
+  computed: {
+    uid() {
+      return String(this.$.uid);
+    },
+  },
+};
+export default TodoItem;
+</script>
