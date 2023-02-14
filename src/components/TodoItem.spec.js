5aca1f7f9b00b474944005183f8f3085eea2632c
gsuke <54441258+gsuke@users.noreply.github.com>
2023-02-14 23:45:42 +0900

    refactor: TodoItem: categorize tests

--- src/components/TodoItem.spec.js
+++ src/components/TodoItem.spec.js
@@ -6,38 +6,42 @@ describe("TodoItem", () => {
   afterEach(cleanup);
 
   function renderTodoItem(isDone = false, text = "Shopping") {
-    render(TodoItem, { props: { isDone, text } });
+    return render(TodoItem, { props: { isDone, text } });
   }
 
-  it("renders a checkbox", () => {
-    renderTodoItem();
-    expect(screen.queryByRole("checkbox")).toBeTruthy();
+  describe("layout", () => {
+    it("renders a checkbox", () => {
+      renderTodoItem();
+      expect(screen.queryByRole("checkbox")).toBeTruthy();
+    });
+
+    it("renders a received ToDo text", () => {
+      renderTodoItem(false, "Shopping");
+      expect(screen.queryByText("Shopping")).toBeTruthy();
+    });
+
+    it("renders a checked checkbox if the task is done", () => {
+      renderTodoItem(true);
+      expect(screen.getByRole("checkbox")).toBeChecked();
+    });
+
+    it("renders a unchecked checkbox if the task is not done", () => {
+      renderTodoItem(false);
+      expect(screen.getByRole("checkbox")).not.toBeChecked();
+    });
   });
 
-  it("renders a received ToDo text", () => {
-    renderTodoItem(false, "Shopping");
-    expect(screen.queryByText("Shopping")).toBeTruthy();
-  });
+  describe("interaction", () => {
+    it("emits click event and checked checkbox status when the unchecked checkbox is clicked", async () => {
+      const { getByRole, emitted } = renderTodoItem(false);
+      await fireEvent.click(getByRole("checkbox"));
+      expect(emitted("change")[0][0]).toBeTruthy();
+    });
 
-  it("renders a checked checkbox if the task is done", () => {
-    renderTodoItem(true);
-    expect(screen.getByRole("checkbox")).toBeChecked();
-  });
-
-  it("renders a unchecked checkbox if the task is not done", () => {
-    renderTodoItem(false);
-    expect(screen.getByRole("checkbox")).not.toBeChecked();
-  });
-
-  it("emits click event and checked checkbox status when the unchecked checkbox is clicked", async () => {
-    const { getByRole, emitted } = renderTodoItem(false);
-    await fireEvent.click(getByRole("checkbox"));
-    expect(emitted("change")[0][0]).toBeTruthy();
-  });
-
-  it("emits click event and unchecked checkbox status when the checked checkbox is clicked", async () => {
-    const { getByRole, emitted } = renderTodoItem(true);
-    await fireEvent.click(getByRole("checkbox"));
-    expect(emitted("change")[0][0]).toBeFalsy();
+    it("emits click event and unchecked checkbox status when the checked checkbox is clicked", async () => {
+      const { getByRole, emitted } = renderTodoItem(true);
+      await fireEvent.click(getByRole("checkbox"));
+      expect(emitted("change")[0][0]).toBeFalsy();
+    });
   });
 });
