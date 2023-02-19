b2f0fd1080a914c757e8cb43c3c666a3837eb7a7
gsuke <54441258+gsuke@users.noreply.github.com>
2023-02-19 17:58:16 +0900

    feat: App: style

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,20 @@ export default App;
 </script>
 
 <template>
-  <div class="form-control">
-    <div class="input-group">
-      <input
-        type="text"
-        placeholder="例) 買い物に行く"
-        class="input input-bordered"
-        aria-label="New ToDo text"
-      />
-      <button class="btn btn-square px-1">追加</button>
+  <div class="mx-auto max-w-lg p-3">
+    <!-- Add ToDo -->
+    <div class="form-control mb-3">
+      <div class="input-group">
+        <input
+          type="text"
+          placeholder="例) 買い物に行く"
+          class="input input-bordered w-full"
+          aria-label="New ToDo text"
+        />
+        <button class="btn btn-square px-1">追加</button>
+      </div>
     </div>
+
+    <TodoList :todo-list="todoList" />
   </div>
-  <TodoList :todo-list="todoList" />
 </template>
